7de3c04f7601c88c8278ffb86d3bd70c6ae27db3
Jack Madden <jck13mad@gmail.com>
2020-06-02 00:09:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     if Time.now.hour > 12
-      resp.write "Good Afternoon"
+      resp.write "Good Afternoon!"
     else
-      
+      resp.write "Good Morning!"
+    end
   
   
   
